5a1a2185532ce0606ebad92e5e8c20259507e6a7
huongbtph16610 <buithihuong20080@gmail.com>
2022-05-09 11:17:49 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular_web15304';
+  title = 'angular_web16304';
+
+  //phần logic, định nghĩa giá trị biến và sử dụng ở html
+  name = 'huongbtph16610';
+  age = '20';
+  //kiểu dữ liệu mảng
+  students = [
+    {
+      id: '1',
+      name: 'student 1',
+      status: 0
+    },
+    {
+      id: '2',
+      name: 'student 2',
+      status: 1
+
+    }
+  ];
+
 }
