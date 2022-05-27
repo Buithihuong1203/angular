0a77debce9ea3fddc71253ff265b666ec19bf847
Buihuong1203 <huongbtph16610@fpt.edu.vn>
2022-05-27 09:46:05 +0700

    routers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,8 @@ import { FormsModule } from '@angular/forms';
 import { UserComponent } from './user/user.component';
 import { UserListComponent } from './user/user-list/user-list.component';
 import { UserFormComponent } from './user/user-form/user-form.component';
+import { HomeComponent } from './home/home.component';
+import { ClientLayoutComponent } from './layouts/client-layout/client-layout.component';
 
 
 
@@ -37,6 +39,8 @@ import { UserFormComponent } from './user/user-form/user-form.component';
     UserComponent,
     UserListComponent,
     UserFormComponent,
+    HomeComponent,
+    ClientLayoutComponent,
 
     //FormComponentComponent
   ],
