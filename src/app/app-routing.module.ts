80f657410857bd7aaee8928419f68d0ea1f0249a
Buihuong1203 <huongbtph16610@fpt.edu.vn>
2022-06-06 11:18:24 +0700

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CanAccessAdminGuard } from './guards/can-access-admin.guard';
 import { HomeComponent } from './home/home.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { ClientLayoutComponent } from './layouts/client-layout/client-layout.component';
 import { AdminProductDetailComponent } from './pages/admin/admin-product/admin-product-detail/admin-product-detail.component';
 import { AdminProductFormComponent } from './pages/admin/admin-product/admin-product-form/admin-product-form.component';
 import { AdminProductListComponent } from './pages/admin/admin-product/admin-product-list/admin-product-list.component';
+import { LoginComponent } from './pages/auth/login/login.component';
 import { UserFormComponent } from './user/user-form/user-form.component';
 import { UserComponent } from './user/user.component';
 
@@ -27,6 +29,7 @@ const routes: Routes = [
   {
     path: 'admin',
     component: AdminLayoutComponent,
+    canActivate: [CanAccessAdminGuard] , //đưa vào để kiểm soát việc login trước khi vào admin
     children: [
       // {
       //   path:'',
@@ -59,9 +62,21 @@ const routes: Routes = [
             component: AdminProductDetailComponent
           } //đẩy admin/product/id xuống dưới cùng tránh việc nhầm id ='create'
         ]
-      }
+      },
+
     ]
 
+  },
+  {
+    path: 'auth',
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent
+      }
+
+
+    ]
   }
   // {
   //   path:'user',
@@ -90,6 +105,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [CanAccessAdminGuard] //đưa vào để route bên trên có thể dùng
 })
 export class AppRoutingModule { }
